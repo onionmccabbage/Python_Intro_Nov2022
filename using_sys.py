128ca18fe30b82e5b58978c37699e13fc0eeecb1
onionmccabbage <onionmccabbage@live.com>
2022-11-08 12:31:16 +0000

    Add files via upload

---
+++ using_sys.py
@@ -0,0 +1,5 @@
+import sys # sys represents the computer system that Python is running on
+
+print(sys.platform) # what is the operating system
+print(sys.version)  # what version of Python is this?
+print(sys.path)     # a list of all the locations Python will look for libraries
